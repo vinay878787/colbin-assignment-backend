81598c7d61235f74cc7200feb5fb83b22659aadb
vinay878787 <vinay878787@gmail.com>
2025-09-24 10:31:44 +0530

    fix: handled CORS issues

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { connectDB } from "./db/db";
 import userRoutes from "./routes/user.routes";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-production-domain.com",
+];
+
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express 🚀");
